31b4468839a39f9bc409a6b39307bdc4dd250129
Rob Little <rlittletht@users.noreply.github.com>
2018-11-04 17:09:05 -0800

    add call to localhost webapi2

--- WebApp/Default.aspx.cs
+++ WebApp/Default.aspx.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -17,6 +22,21 @@ namespace WebApp
         protected void DoCallService(object sender, EventArgs e)
         {
             divOutput.InnerHtml += "DoCallService Called<br/>";
+
+            HttpClient client = new HttpClient();
+
+            Task<HttpResponseMessage> resp = client.GetAsync("http://localhost/webapisvc/api/websvc/test");
+
+            resp.Wait();
+            string s;
+
+            using (MemoryStream stm = new MemoryStream())
+            {
+                resp.Result.Content.CopyToAsync(stm).Wait();
+                s = Encoding.UTF8.GetString(stm.ToArray());
+            }
+
+            divOutput.InnerHtml += $"Response = {s}<br/>";
         }
     }
 }
